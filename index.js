c68f14e56d9b92d00755a9d426bbc68404a6a692
KrishnaprabhaCM <102539266+KrishnaprabhaCM@users.noreply.github.com>
2022-07-14 21:10:52 +0530

    version3

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const express = require('express');
 const path = require ('path'); 
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const port = 3000;
 
 // const nav= [
 //     {
