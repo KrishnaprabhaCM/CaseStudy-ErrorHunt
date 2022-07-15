1ecb9b51c1267c2c960c6d87bcf53bb889f97bf5
KrishnaprabhaCM <102539266+KrishnaprabhaCM@users.noreply.github.com>
2022-07-15 19:50:26 +0530

    new commit

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-// url = mongoose.connect(process.env.MONGODB_URI || 'mongodb://:localhost27017/Library');
-let url = process.env.MONGODB_URI || 'mongodb://:localhost27017/Library';
-mongoose.connect(url, { useMongoClient: true });
+url = mongoose.connect(process.env.MONGODB_URI || 'mongodb://:localhost27017/Library');
+// let url = process.env.MONGODB_URI || 'mongodb://:localhost27017/Library';
+// mongoose.connect(url, { useMongoClient: true });
 const Schema = mongoose.Schema;
 
 
--- src/model/BookModel.js
+++ src/model/BookModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Library');
-let url = process.env.MONGODB_URI || 'mongodb://:localhost27017/Library';
-mongoose.connect(url, { useMongoClient: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Library');
+// let url = process.env.MONGODB_URI || 'mongodb://:localhost27017/Library';
+// mongoose.connect(url, { useMongoClient: true });
 const Schema = mongoose.Schema;
 
 
