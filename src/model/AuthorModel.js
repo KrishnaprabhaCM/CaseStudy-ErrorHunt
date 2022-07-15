d6a85e5bf4ef889be2fdd4faa1a7526dd4abcea1
KrishnaprabhaCM <102539266+KrishnaprabhaCM@users.noreply.github.com>
2022-07-15 19:53:08 +0530

    new commit

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 url = mongoose.connect(process.env.MONGODB_URI || 'mongodb://:localhost27017/Library');
-// let url = process.env.MONGODB_URI || 'mongodb://:localhost27017/Library';
-// mongoose.connect(url, { useMongoClient: true });
 const Schema = mongoose.Schema;
 
 
